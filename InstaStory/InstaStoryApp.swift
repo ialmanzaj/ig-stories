d8b58f507e4a28c14a62371acb9f6531f1721669
“Isaac” <isaac.almanza19@gmail.com>
2025-09-25 18:59:29 -0500

    Initial commit for checkpoints

---
+++ InstaStory/InstaStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaStoryApp.swift
+//  InstaStory
+//
+//  Created by Isaac on 09/25/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
