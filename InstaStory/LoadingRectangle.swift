1df99f0eb2f37735df13da83cd8c7e2d113587d7
“Isaac” <isaac.almanza19@gmail.com>
2025-09-26 16:09:09 -0500

    feat: add ui tests

--- InstaStory/LoadingRectangle.swift
+++ InstaStory/LoadingRectangle.swift
@@ -9,22 +9,24 @@
 import SwiftUI
 
 struct LoadingRectangle: View {
-    
+
     var progress: CGFloat
-    
+
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle()
                     .foregroundColor(Color.white.opacity(0.3))
                     .cornerRadius(5)
-                
+
                 Rectangle()
                     .frame(width: geometry.size.width * self.progress, height: nil, alignment: .leading)
                     .foregroundColor(Color.white.opacity(0.9))
                     .cornerRadius(5)
             }
         }
+        .accessibilityElement(children: .ignore)
+        .accessibilityAddTraits(.isButton)
     }
 }
 
